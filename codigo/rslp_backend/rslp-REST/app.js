4ba844f5cae4540a6424cdfbcd30699e53ce3697
darioaplicano <aplicano0921@gmail.com>
2019-03-07 17:55:58 +0100

    Agregando al backend los contenidos

--- codigo/rslp_backend/rslp-REST/app.js
+++ codigo/rslp_backend/rslp-REST/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/usuario.routes');
+var contentsRouter = require('./routes/contenido.routes');
 
 const mongoose = require('mongoose');
 
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/usuario', usersRouter);
+app.use('/contenido', contentsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
