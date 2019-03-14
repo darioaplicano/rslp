4e205139089f892237220a6d3d58259acd9152ad
cathave <47638875+cathave@users.noreply.github.com>
2019-03-14 19:29:28 +0100

    Add files via upload

---
+++ codigo/rslp_backend/rslp-REST/routes/comentar.routes.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+const contenido = require('../controllers/comentar.controller.js');
+
+/* GET contenidos. */
+router.get('/', contenido.findAll);
+router.post('/', contenido.create);
+router.get('/:contenid', contenido.findOne);
+
+module.exports = router;
