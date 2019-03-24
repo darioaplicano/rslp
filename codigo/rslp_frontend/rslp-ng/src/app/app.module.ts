4c524e889828624390c511eb47a0a46ee602b4fe
es2812 <es2812@hotmail.com>
2019-03-25 00:48:20 +0100

    Added nav buttons to mainbar

--- codigo/rslp_frontend/rslp-ng/src/app/app.module.ts
+++ codigo/rslp_frontend/rslp-ng/src/app/app.module.ts
@@ -6,23 +6,27 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatGridListModule } from '@angular/material/grid-list'
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatDividerModule } from '@angular/material/divider';
 import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MainComponent } from './main/main.component';
 import { HttpClientModule } from '@angular/common/http';
 import { DataService } from './data.service';
 import { RegistroComponent } from './registro/registro.component';
+import { UserconfigComponent } from './userconfig/userconfig.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     MainComponent,
-    RegistroComponent
+    RegistroComponent,
+    UserconfigComponent
   ],
   imports: [
     BrowserModule,
@@ -35,6 +39,8 @@ import { RegistroComponent } from './registro/registro.component';
     MatGridListModule,
     HttpClientModule,
     AppRoutingModule,
+    MatIconModule,
+    MatDividerModule,
     FormsModule
   ],
   providers: [DataService],
